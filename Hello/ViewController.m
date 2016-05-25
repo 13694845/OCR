ca24f8041bed7a492d5cb667d862b9593a88f80d
Your Name <you@example.com>
2016-05-25 13:47:03 +0800

    .

--- Hello/ViewController.m
+++ Hello/ViewController.m
@@ -17,6 +17,44 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    // UIImage *image = [UIImage imageNamed:@"ghost_tiny"];
+    UIImage *image = [UIImage imageNamed:@"blue"];
+    
+    CGImageRef inputCGImage = [image CGImage];
+    NSUInteger width = CGImageGetWidth(inputCGImage);
+    NSUInteger height = CGImageGetHeight(inputCGImage);
+    
+    NSUInteger bytesPerPixel = 4;
+    NSUInteger bytesPerRow = bytesPerPixel * width;
+    NSUInteger bitsPerComponent = 8;
+    
+    UInt32 *pixels = (UInt32 *)calloc(width * height, sizeof(UInt32));
+    
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    CGContextRef context = CGBitmapContextCreate(pixels, width, height,
+                                                 bitsPerComponent, bytesPerRow, colorSpace,
+                                                 kCGImageAlphaPremultipliedLast|kCGBitmapByteOrder32Big);
+    
+    CGContextDrawImage(context, CGRectMake(0, 0, width, height), inputCGImage);
+    
+    CGColorSpaceRelease(colorSpace);
+    CGContextRelease(context);
+    
+    #define Mask8(x) ( (x) & 0xFF )
+    #define R(x) ( Mask8(x) )
+    #define G(x) ( Mask8(x >> 8 ) )
+    #define B(x) ( Mask8(x >> 16) )
+    
+    UInt32 *currentPixel = pixels;
+    for (NSUInteger j = 0; j < height; j++) {
+        for (NSUInteger i = 0; i < width; i++) {
+            UInt32 color = *currentPixel;
+            printf("%3.0f ", (R(color)+G(color)+B(color))/3.0);
+            currentPixel++;
+        }
+        printf("\n");
+    }
+    free(pixels);
 }
 
 - (void)didReceiveMemoryWarning {

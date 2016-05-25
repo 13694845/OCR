9ee0f88567c980ed4086d0f33b118206fa20ffb9
Your Name <you@example.com>
2016-05-25 11:03:58 +0800

    Initial Commit

---
+++ Hello/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hello
+//
+//  Created by QSH on 16/5/25.
+//  Copyright © 2016年 Zen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hello/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hello
+//
+//  Created by QSH on 16/5/25.
+//  Copyright © 2016年 Zen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

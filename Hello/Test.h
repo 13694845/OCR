8d92ae59f75d5f293f5b2419b7da2dec3fa464a5
Your Name <you@example.com>
2016-05-31 16:52:14 +0800

    .

---
+++ Hello/Test.h
@@ -0,0 +1,14 @@
+//
+//  Test.h
+//  Hello
+//
+//  Created by QSH on 16/5/31.
+//  Copyright © 2016年 Zen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#include "CImg.h"
+
+@interface Test : NSObject
+
+@end
---
+++ Hello/Test.mm
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  Hello
+//
+//  Created by QSH on 16/5/31.
+//  Copyright © 2016年 Zen. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end

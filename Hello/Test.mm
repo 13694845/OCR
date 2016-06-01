d93a45d77fa09306bd15405a246ed0b3685e2cdf
Your Name <you@example.com>
2016-06-01 15:26:38 +0800

    .

--- Hello/Test.mm
+++ Hello/Test.mm
@@ -12,19 +12,23 @@
 @implementation Test
 
 - (int)similarityOfImage:(NSString *)imageA andImage:(NSString *)imageB {
-    NSLog(@"\n\n Image A : %@", imageA);
     ulong64 hash1;
     const char *path1 = [imageA UTF8String];
     ph_dct_imagehash(path1, hash1);
     
-    NSLog(@"\n\n Image B : %@", imageB);
     ulong64 hash2;
     const char *path2 = [imageB UTF8String];
     ph_dct_imagehash(path2, hash2);
     
     int distance = -1;
     distance = ph_hamming_distance(hash1, hash2);
-    NSLog(@"\n\n distance : %d", distance);
+    
+    printf("\n\n");
+    printf("**************************\n");
+    NSLog(@"hash a : %llu\n", hash1);
+    NSLog(@"hash b : %llu\n", hash2);
+    NSLog(@"distance : %d", distance);
+    printf("**************************\n");
     
     return distance;
 }

da1ca1aae82d6d9d03f01989f77cdb0e5ffacf75
Your Name <you@example.com>
2016-06-02 10:11:03 +0800

    .

---
+++ Hello/SRImage.h
@@ -0,0 +1,13 @@
+//
+//  SRImage.h
+//  Hello
+//
+//  Created by QSH on 16/6/2.
+//  Copyright © 2016年 Zen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SRImage : UIImage
+
+@end
---
+++ Hello/SRImage.m
@@ -0,0 +1,13 @@
+//
+//  SRImage.m
+//  Hello
+//
+//  Created by QSH on 16/6/2.
+//  Copyright © 2016年 Zen. All rights reserved.
+//
+
+#import "SRImage.h"
+
+@implementation SRImage
+
+@end
